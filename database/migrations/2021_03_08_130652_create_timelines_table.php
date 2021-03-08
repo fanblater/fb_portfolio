78a66217e3d149c2fea9259b0d69c2ba37254fe1
fanblater <fanny.bellais@outlook.com>
2021-03-08 17:13:31 +0100

    create table timeline

---
+++ database/migrations/2021_03_08_130652_create_timelines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimelinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timelines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->string('formation');
+            $table->string('titre');
+            $table->string('projets');
+            $table->string('projets_annexes');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timelines');
+    }
+}
