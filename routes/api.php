03122d4d2fe92c20b0be138624fd423a4b2e283f
fanblater <fanny.bellais@outlook.com>
2021-03-17 17:29:12 +0100

    update route laravel

--- routes/api.php
+++ routes/api.php
@@ -30,3 +30,6 @@ Route::post('create', [TimelineController::class, 'store']);
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout']);
+Route::delete('delete/{id}', [TimelineController::class, 'destroy']);
+Route::put('update/{id}', [TimelineController::class, 'update']);
+Route::get('timeline/{id}', [TimelineController::class, 'show']);
