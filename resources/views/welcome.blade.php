d99de72d3c1e6234dd503e1e7cce70ffcab7c87e
fanblater <fanny.bellais@outlook.com>
2021-03-17 17:30:40 +0100

    update navigation view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,47 +12,57 @@
 
 <body>
     <div id="app">
-        <div>
-            <header>
-                <div class="container-nav">
+        <div class="container-fluid h-100">
+            <div class="row">
+                <div class="col-sm-12">
+                    <router-link to="/"><img class="logo" src="{{asset('img/logo_fb_v1.svg')}}" alt="logo"></router-link>
 
-                    <div class="nav">
-                        <nav>
-                                <ul>
-                                    <li>
-                                        <router-link to="/">Accueil</router-link>
-                                    </li>
-                                    <li>
-                                        <router-link to="/timeline">Timeline</router-link>
-                                    </li>
-                                    <li>
-                                        <router-link to="/contact">Contact</router-link>
-                                    </li>
-                                    <li>
-                                        <router-link to="/quizz">Quizz</router-link>
-                                    </li>
-                                    <li>
-                                        <router-link to="/login">Log in</router-link>
-                                    </li>
-                                    {{-- <li>
-                                        <router-link to="/register">Register</router-link>
-                                    </li> --}}
-                                    <li><a href="www.linkedin.com/in/fanny-bellais">Linkdin</a></li>
-                                    <li><a href="https://github.com/fanblater">Github</a></li>
-                                </ul>
+                </div>
+            </div>
 
-                        </nav>
+            <div class="row justify-content-center align-items-center h-100">
+                <div class="col-sm-10">
+                    <div class="container">
+                        <router-view></router-view>
                     </div>
                 </div>
+                <div class="col-sm-2">
+                    <header>
 
-            </header>
+                                <nav>
+                                    <ul>
+                                        <li>
 
+                                        </li>
+                                        <li>
+                                            <router-link to="/timeline">Timeline</router-link>
+                                        </li>
+                                        <li>
+                                            <router-link to="/contact">Contact</router-link>
+                                        </li>
+                                        <li>
+                                            <router-link to="/quizz">Quizz</router-link>
+                                        </li>
+                                        <li>
+                                            <router-link to="/login">Log in</router-link>
+                                        </li>
+                                        {{-- <li>
+                                                            <router-link to="/register">Register</router-link>
+                                                        </li> --}}
+                                        <li><a href="www.linkedin.com/in/fanny-bellais">Linkdin</a></li>
+                                        <li><a href="https://github.com/fanblater">Github</a></li>
+                                    </ul>
+
+                                </nav>
+
+                    </header>
+                </div>
+            </div>
         </div>
 
 
-<div class="content">
-    <router-view></router-view>
-</div>
+
+
     </div>
 
 
