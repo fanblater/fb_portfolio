56c8ee2654ecff65e650211afef51ee762edec09
fanblater <fanny.bellais@outlook.com>
2021-04-05 17:26:31 +0200

    update db for quiz

---
+++ database/migrations/2021_04_05_095512_quizz_question.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class QuizzQuestion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('question');
+        Schema::enableForeignKeyConstraints();
+        Schema::create('question', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question');
+            $table->boolean('is_active');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question');
+    }
+}
---
+++ database/migrations/2021_04_05_131124_quizz_choix.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class QuizzChoix extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('choix');
+        Schema::enableForeignKeyConstraints();
+        Schema::create('choix', function (Blueprint $table) {
+            $table->increments('id',true);
+            
+            $table->unsignedInteger('id_question');
+            $table->boolean('is_valide');
+            $table->string('choix');
+            $table->foreign('id_question')->references('id')->on('question')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('choix');
+    }
+}
